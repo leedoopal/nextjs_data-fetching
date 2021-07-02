9d8a2775d2855a5bd496ffd969af3285f33a41f7
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-02 23:04:24 +0900

    pre-fetching과 client-side fetching를 합침

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-function LastSalesPage() {
-  const [sales, setSales] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  const LAST_SALES_BASE_URL =
-    'https://nextjs-react-lv2-default-rtdb.firebaseio.com/sales.json';
-
-  const { data, error } = useSWR(LAST_SALES_BASE_URL);
+  const { data, error } = useSWR(
+    'https://nextjs-react-lv2-default-rtdb.firebaseio.com/sales.json',
+  );
 
   useEffect(() => {
     if (data) {
@@ -26,32 +25,11 @@ function LastSalesPage() {
     }
   }, [data]);
 
-  /*useEffect(() => {
-    setIsLoading(true);
-
-    fetch(LAST_SALES_BASE_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        const transformedSales = [];
-
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);*/
-
   if (error) {
     return <p>Failed to load.</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -64,6 +42,26 @@ function LastSalesPage() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://nextjs-react-lv2-default-rtdb.firebaseio.com/sales.json',
+  );
+  const data = await res.json();
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  // revalidate: 페이지 re-generate가 발생할 수 있는 시간
+  // 요청이 들어올 때 10초마다 page를 re-generation 함.
+  return { props: { sales: transformedSales }, revalidate: 10 };
 }
 
 export default LastSalesPage;
